ec374064402ad827354a339327eb33191387b35e
kwakdony <kwakdh25@gmail.com>
2021-03-28 23:41:58 +0900

    ADD

--- pythonProject/blog/views.py
+++ pythonProject/blog/views.py
@@ -1,14 +1,17 @@
 from django.views.generic import ListView, DetailView, TemplateView
+from django.views.generic import CreateView, UpdateView, DeleteView, FormView
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic.dates import ArchiveIndexView, YearArchiveView, MonthArchiveView
 from django.views.generic.dates import DayArchiveView, TodayArchiveView
 from django.conf import settings
-
-from blog.models import Post
-
-from django.views.generic import FormView
-from blog.forms import PostSearchForm
 from django.db.models import Q
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from mysite.views import OwnerOnlyMixin
+
+from blog.models import Post
+from blog.forms import PostSearchForm
+
 
 class PostLV(ListView):
     model = Post
@@ -77,3 +80,30 @@ class SearchFormView(FormView):
         context['object_list'] = post_list
 
         return render(self.request, self.template_name, context) # No Redirection
+
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    fields = ['title', 'slug', 'description', 'connect', 'tags']
+    initial = {'slug': 'auto-filling-do-not-input'}
+    #fields = ['title', 'description', 'content', 'tags']
+    success_url = reverse_lazy('blog:index')
+
+    def form_valid(self, form):
+        form.instance.owner = self.request.user
+        return super().form_valid(form)
+
+class PostChangeLV(LoginRequiredMixin, ListView):
+    template_name  = 'blog/post_change_list.html'
+
+    def get_queryset(self):
+        return Post.objects.filter(owner=self.request.user)
+
+class PostUpdateView(OwnerOnlyMixin, UpdateView):
+    model = Post
+    fields = ['title', 'slug', 'description', ' content', 'tags']
+    success_url = reverse_lazy('blog:index')
+
+class PostDeleteView(OwnerOnlyMixin, DeleteView):
+    model = Post
+    succes_url = reverse_lazy('blog:index')
+    
